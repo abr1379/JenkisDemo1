d9bb911e91ce63de8018881da6d04c0ab64d7fd1
abr1379 <245122748008@mvsrec.edu.in>
2025-03-18 12:11:59 +0530

    Jcommit2

---
+++ cal.java
@@ -0,0 +1,13 @@
+class cal
+{
+public static void main(String args[])
+{
+int a,b;
+a=10;
+b=15;
+System.out.println(a+b);
+System.out.println(a-b);
+System.out.println(a/b);
+System.out.println(a*b);
+}
+}
